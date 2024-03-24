92d284f53108c388ce22fbb51cd5b60c6828e8e1
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-03-24 20:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", downloadAndDisplayImages);
+
+function downloadAndDisplayImages() {
+  Promise.all(images.map(downloadImage))
+    .then(displayImages)
+    .catch((error) => console.error(error));
+}
+
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () =>
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    img.src = image.url;
+  });
+}
+
+function displayImages(images) {
+  output.innerHTML = "";
+  images.forEach((img) => {
+    const link = document.createElement("a");
+    link.href = img.src;
+    link.download = true;
+    const imgElement = document.createElement("img");
+    imgElement.src = img.src;
+    imgElement.alt = "Downloaded Image";
+    link.appendChild(imgElement);
+    output.appendChild(link);
+  });
+}
